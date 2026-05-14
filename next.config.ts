38556b366b1637408182e85170e2181ca0d59962
vigneshwaran <vigneshwaran.manivannan@ticktix.com>
2026-05-14 09:58:26 +0530

    nextjs version updated

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,6 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  output: "standalone",
 
   // ✅ Allow local network access from mobile (dev only)
   allowedDevOrigins: ['http://172.31.1.43:3000'],
