dab16a90207f33966782dec7bc443d5b6895a7da
vigneshwaran <vigneshwaran.manivannan@ticktix.com>
2026-05-14 09:52:14 +0530

    build fix

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  output: "standalone",
 
   // ✅ Allow local network access from mobile (dev only)
   allowedDevOrigins: ['http://172.31.1.43:3000'],
